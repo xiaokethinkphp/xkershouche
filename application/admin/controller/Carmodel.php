4f51e8e1a35f13b6fff58451aa5fc463c6db5fc7
xiaokethinkphp <425532268@qq.com>
2018-05-17 20:58:38 +0800

    车型管理
    车型添加

--- application/admin/controller/Carmodel.php
+++ application/admin/controller/Carmodel.php
@@ -9,12 +9,23 @@ class Carmodel extends Controller
     // 车型列表显示
     public function lst()
     {
+        $alpha = array('A','B','C','D','E',
+        'F','G','H','I','J','K','L','M','N',
+        'O','P','Q','R','S','T','U','V','W','X','Y','Z');
+        $this->assign('az',$alpha);
         return view();
     }
 
     // 添加车型界面显示
     public function add()
     {
+        // 获取当前年份
+        $current_year = date("Y");
+        $years = array();
+        for ($i=0; $i < 20; $i++) {
+            $years[$i] = $current_year-$i;
+        }
+        $this->assign("years",$years);
         return view();
     }
 
@@ -30,9 +41,33 @@ class Carmodel extends Controller
 
     }
 
+    // 车型添加提交方法
     public function addHanddle()
     {
         $post = request()->post();
-        dump($post);
+        $validate = validate("Carmodel");
+        if (!$validate->check($post)) {
+            $this->error($validate->getError());
+        }
+        $carmodel_insert_result = db("carmodel")->insert($post);
+        if ($carmodel_insert_result) {
+            $this->success("车型添加成功",'admin/carmodel/lst');
+        }else {
+            $this->error("车型添加失败",'admin/carmodel/lst');
+        }
+    }
+
+
+    // 验证表单的ajax方法
+    public function checkEditionAjax()
+    {
+        if (request()->isAjax()) {
+            $post = request()->post();
+            $validate = validate("Carmodel");
+            return $validate->check($post);
+        } else {
+            $this->redirect("admin/carmodel/lst");
+        }
+
     }
 }
