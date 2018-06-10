a77da11fe3b22cdcd171a7d836d0bdc83df37f1e
xiaokethinkphp <425532268@qq.com>
2018-06-10 17:25:23 +0800

    二手车

--- application/index/controller/Member.php
+++ application/index/controller/Member.php
@@ -93,4 +93,30 @@ class Member extends Common
         }
 
     }
+
+    public function carsuploads()
+    {
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('file');
+        // 移动到框架应用根目录/uploads/ 目录下
+        $info = $file->move( '../uploads');
+        if($info){
+            // 成功上传后 获取上传信息
+            // 输出 jpg
+            echo $info->getExtension();
+            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            echo $info->getSaveName();
+            // 输出 42a79759f284b767dfcb2a0197904287.jpg
+            echo $info->getFilename();
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
+    }
+
+    public function webuploader()
+    {
+        return view();
+    }
+
 }
