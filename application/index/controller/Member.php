02ce8470ce1b58550f4b2f7c75bace0522deb541
xiaokethinkphp <425532268@qq.com>
2018-06-10 19:21:42 +0800

    二手车

--- application/index/controller/Member.php
+++ application/index/controller/Member.php
@@ -99,7 +99,7 @@ class Member extends Common
         // 获取表单上传文件 例如上传了001.jpg
         $file = request()->file('file');
         // 移动到框架应用根目录/uploads/ 目录下
-        $info = $file->move( '../uploads');
+        $info = $file->move( '../uploads/webuploaderdemo');
         if($info){
             // 成功上传后 获取上传信息
             // 输出 jpg
