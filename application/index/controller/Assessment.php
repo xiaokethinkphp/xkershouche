f6666e08ef1dd1c70facee7442c82d2a129bf889
xiaokethinkphp <425532268@qq.com>
2018-07-12 15:40:39 +0800

    二手车

---
+++ application/index/controller/Assessment.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\index\controller;
+use \think\Controller;
+/**
+ * 车源评估控制器
+ */
+class Assessment extends Controller
+{
+    public function index()
+    {
+        return view();
+    }
+
+}
